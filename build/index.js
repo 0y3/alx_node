1cf69ab211feebf119c66bee2e45b2246f5ccc68
0y3 <trivin98@gmail.com>
2022-12-16 16:25:42 +0100

    new

---
+++ build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const showMenu = () => {
+    return `Select Mocha from menu: \n
+       1: Create White Chocolate Mocha \n
+       2: Create Dark Chocolate Mocha \n
+       3: Create Peppermint Mocha\n
+       0: Exit\n`;
+};
+console.log(showMenu());
